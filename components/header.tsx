a85b3b1c0f172ce84c33db280408828e10bb69d2
dioKR <azss0470@gmail.com>
2024-07-18 15:23:47 +0900

    [FEAT] BrowserButtons 호출 In header

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,9 @@
+import BrowserButtons from './browser/browser-buttons';
+
 export default function Header() {
   return (
-    <header className="bg-black2 border-strokedark2 h-10 w-full border-b font-semibold"></header>
+    <header className="bg-black2 border-strokedark2 h-10 w-full border-b font-semibold">
+      <BrowserButtons />
+    </header>
   );
 }
